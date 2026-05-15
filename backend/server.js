529eb972aea86ccd467b7dd3d0dceb51a08ffaec
Anish <anish.k.m2005@gmail.com>
2026-05-16 00:22:12 +0530

    fix the authentication

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-dotenv.config(); // Try current dir
+dotenv.config({ path: path.join(__dirname, '.env') });
 if (!process.env.MONGO_URI) {
   dotenv.config({ path: path.join(__dirname, '../.env') });
 }
